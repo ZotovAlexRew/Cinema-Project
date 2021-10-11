fb1bf6563d81e53950195756dee4388334611edc
ZotovAlexRew <92159346+ZotovAlexRew@users.noreply.github.com>
2021-10-11 10:10:34 +0300

    Не большие поправки

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 750;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -9,4 +11,8 @@ schemeSvg.addEventListener("click", (event) => {
     totalPrice = totalSeats * cost;
     totalPriceTag.textContent = totalPrice;
   }
+});
+menuButton.addEventListener("click" , () => {
+ console.log('Кликнули по меню');
+ menu.classList.toggle("is-open");
 });
